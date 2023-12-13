4bcba9c675d9467b2ba33f47f5c96f3f06936dae
lucas cortez <lucasc.sanches65@gmail.com>
2023-12-12 23:11:10 -0300

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import { useTestStore } from "@/store/testStore";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const increment = useTestStore((state) => (state as { increment: () => void }).increment);
   const count = useTestStore((state) => (state as { count: number }).count);
+  const router = useRouter();
 
   useEffect(() => {
     increment();
@@ -16,7 +18,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col h-full bg-black text-white items-center justify-center">
       <p>hello: {count}</p>
       <button onClick={() => increment()}>add</button>
-      <Link href={"/dashboard"}>dashboard</Link>
+      <Link href={"/dashboard"}>dashboard(link)</Link>
+      <button onClick={() => router.push("/dashboard")}>dashboard(router)</button>
     </main>
   );
 }
